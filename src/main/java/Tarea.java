59c20ebc5b10f7b0edcc461ae91de8474ae8abbe
Daniel <daxyt98@gmail.com>
2021-03-02 11:13:04 +0100

    primera

--- src/main/java/Tarea.java
+++ src/main/java/Tarea.java
@@ -1,2 +1,17 @@
+import java.util.List;
+
 public class Tarea {
+    String titulo;
+    String descripcion;
+    List<Persona> personas;
+    Persona responsable;
+    int prioridad;
+    String fechaIni;
+    String fechaFin;
+    Boolean realizada;
+    Resultado resultado;
+    List<String> etiquetas;
+
+    public Tarea(){}
+
 }
