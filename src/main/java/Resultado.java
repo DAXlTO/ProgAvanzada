fdf58905688750d59a537146d6bd3f8c13494e4d
SergiioJM <sergiojimenez10@hotmail.com>
2021-03-02 11:17:21 +0100

    Clase Resultado S

--- src/main/java/Resultado.java
+++ src/main/java/Resultado.java
@@ -1,2 +1,11 @@
 public class Resultado {
+    String identificador;
+    Integer horas;
+    String internoOcomercial;
+
+    public Resultado(String identificador, Integer horas, String internoOcomercial) {
+        this.identificador = identificador;
+        this.horas = horas;
+        this.internoOcomercial = internoOcomercial;
+    }
 }
