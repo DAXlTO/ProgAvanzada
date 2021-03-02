4fad25cbea054b3dbfae1704c02b0b8208d69429
Daniel <daxyt98@gmail.com>
2021-03-02 12:00:01 +0100

    proyecto

---
+++ src/main/java/Proyecto.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Proyecto {
+    String nombre;
+    private List<Tarea> tareas;
+    private List<Persona> personas;
+
+    public Proyecto(String nombre){
+        this.nombre = nombre;
+    }
+
+}
