f14a75bf7db325bdc49aa06af702e2da83d98804
SergiioJM <sergiojimenez10@hotmail.com>
2021-03-02 11:12:28 +0100

    Clase Persona S

--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -1,2 +1,13 @@
+import java.util.List;
+
 public class Persona {
+    String nombre;
+    String email;
+    List<Tarea> lista;
+
+    public Persona(String nombre, String email, List<Tarea> lista){
+        this.nombre = nombre;
+        this.email = email;
+        this.lista = lista;
+    }
 }
