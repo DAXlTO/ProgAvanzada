d74161d100d71fbb5278c005bbc70f9642ce6114
SergiioJM <sergiojimenez10@hotmail.com>
2021-03-02 11:44:10 +0100

    Clase ResultadoPaginaWeb S

--- src/main/java/ResultadoDocumento.java
+++ src/main/java/ResultadoDocumento.java
@@ -1,9 +1,10 @@
 public class ResultadoDocumento extends Resultado{
+
     String formato;
     int numPag;
     float espacio;
 
-    public ResultadoDocumento(String identificador, Integer horas, String internoOcomercial,String formato, int numPag, float espacio) {
+    public ResultadoDocumento(String identificador, int horas, String internoOcomercial,String formato, int numPag, float espacio) {
        super(identificador,horas,internoOcomercial);
        this.formato = formato;
        this.numPag = numPag;
---
+++ src/main/java/ResultadoPaginaWeb.java
@@ -0,0 +1,13 @@
+public class ResultadoPaginaWeb extends Resultado{
+
+    String estaticaOdinamica;
+    String lenguaje;
+    String backend;
+
+    public ResultadoPaginaWeb(String identificador, int horas, String internoOcomercial, String estaticaOdinamica, String lenguaje, String backend) {
+        super(identificador,horas,internoOcomercial);
+        this.estaticaOdinamica = estaticaOdinamica;
+        this.lenguaje = lenguaje;
+        this.backend = backend;
+    }
+}
