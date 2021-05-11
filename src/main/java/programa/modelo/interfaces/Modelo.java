45800334812f9e18aa3d09a0dd26fe2952f64c1d
Daniel <daxyt98@gmail.com>
2021-05-11 12:27:57 +0200

    Clase alta persona funciona

--- src/main/java/programa/modelo/interfaces/Modelo.java
+++ src/main/java/programa/modelo/interfaces/Modelo.java
@@ -1,7 +1,6 @@
 package programa.modelo.interfaces;
 
 import programa.modelo.clases.Persona;
-import programa.modelo.excepciones.PersonaRepetidaException;
 
 public interface Modelo {
     void altaPersona(Persona persona);
